8d06c9dad52b2cbebc18d61f5d5bd0221a5456d1
ABINESH294 <71022377+ABINESH294@users.noreply.github.com>
2025-12-04 13:33:36 +0530

    Change background color from black to green

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import abi from '../public/images/abi.jpg'
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center flex-col space-y-2 bg-black min-h-[calc(100vh-48px)]'>
+    <div className='flex justify-center items-center flex-col space-y-2 bg-green-300 min-h-[calc(100vh-48px)]'>
       <h1 className='font-extrabold text-[20px]'>Hi, I am Abinesh</h1>
       <h2 className='font-bold text-[16px]'>I am a frontend developer</h2>
       <Image src={abi} alt='Not found' width={300} height={300}/>
